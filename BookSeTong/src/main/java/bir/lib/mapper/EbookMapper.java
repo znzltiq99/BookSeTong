e1c672ef5526aa0905e7c3ff06de4d5a147a73d5
sujiKim <student@M40219>
2014-01-07 16:03:01 +0900

    mapper 삽입

--- BookSeTong/src/main/java/bir/lib/mapper/EbookMapper.java
+++ BookSeTong/src/main/java/bir/lib/mapper/EbookMapper.java
@@ -2,21 +2,25 @@ package bir.lib.mapper;
 
 import java.util.List;
 
-import bir.lib.domain.DetailEbookVO;
-import bir.lib.domain.DetailMemberInfoVO;
-import bir.lib.domain.EbookVO;
-import bir.lib.domain.InsertMemberStarVO;
-import bir.lib.domain.Member_starVO;
+import bir.ebook.domain.DetailEbookVO;
+import bir.ebook.domain.DetailMemberInfoVO;
+import bir.ebook.domain.EbookVO;
+import bir.ebook.domain.InsertListVO;
+import bir.ebook.domain.InsertMemberStarVO;
+import bir.ebook.domain.Member_starVO;
 
 
 public interface EbookMapper {
 		public EbookVO getBooks();  
 		public List<EbookVO> getEbooks();
 		public List<DetailEbookVO> getDetailEbooks();
-		public List<Member_starVO> getMemberStar();
+		public List<Member_starVO> getMemberStar(String email);
 		public void insertMemberStar(InsertMemberStarVO vo);
 		public void updateMemberStar(InsertMemberStarVO vo);
 		public DetailEbookVO getDetailEbook(String bnum);
-		public DetailMemberInfoVO getDetailMemberInfo();
+		public DetailMemberInfoVO getDetailMemberInfo(String email);
 		public void updateBmoney(DetailMemberInfoVO vo);
+		public void insertPointList(InsertListVO vo);
+		public void insertPurchaseList(InsertListVO vo);
+		
 }
---
+++ BookSeTong/src/main/java/bir/lib/mapper/Member_infoMapper.java
@@ -0,0 +1,14 @@
+package bir.lib.mapper;
+
+import java.util.List;
+
+import bir.ebook.domain.Member_infoVO;
+
+
+
+
+public interface Member_infoMapper {
+	public void insertMember_info(Member_infoVO vo);
+	public Member_infoVO checkLogin(Member_infoVO vo);
+	
+}
